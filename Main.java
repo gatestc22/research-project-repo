5cb0de60720e9187f89a8dcfa2047a0c141188d9
gatestc22 <gatestc22@gcc.edu>
2023-11-10 04:18:20 -0500

    Display only relevant transmitter and receiver info. Open and connect transmitter and receiver from input and output devices.

--- Main.java
+++ Main.java
@@ -2,10 +2,8 @@
 import javax.sound.midi.*;
 import javax.sound.midi.spi.MidiDeviceProvider;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.util.*;
 
 import static javax.sound.midi.MidiSystem.*;
 
@@ -13,75 +11,85 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        // new code
-        MidiDevice device;
+        // device vars must be initialized outside first for loop
+        MidiDevice deviceIn = null;
+        MidiDevice deviceOut = null;
+        Transmitter midiIn = null;
+        Receiver midiOut = null;
+        Scanner sc;
+        MidiDevice.Info[] infoArray = MidiSystem.getMidiDeviceInfo(); // array of all current midi device info
 
-        Transmitter trans;
-        Receiver rec;
-        Scanner sc = new Scanner(System.in);
-
-        Sequencer seq;
-        Synthesizer syn;
-        int numTrans;
-        int numRec;
-
-        try {
-
-            // obtain information about all recognizable midi devices
-            MidiDevice.Info[] infoArray = MidiSystem.getMidiDeviceInfo();
-            ArrayList<MidiDevice> deviceList = new ArrayList<>();
-            for (int i = 0; i < infoArray.length; i++) {
-                // initialize device variable with info at position i in infoArray
-                device = MidiSystem.getMidiDevice(infoArray[i]);
-                deviceList.add(device);
-                System.out.println("Device: " + device.getDeviceInfo());
+        // find MIDI input device
+        for (int i = 0; i < infoArray.length; i++) {
+            deviceIn = MidiSystem.getMidiDevice(infoArray[i]);
+            if (deviceIn.toString().contains("MidiInDevice")) {
+                System.out.println(deviceIn.toString());
+                deviceIn.open();
+                // print device info
+                System.out.println("~~Device Info~~");
+                System.out.println("Name: " + deviceIn.getDeviceInfo().getName());
+                System.out.println("Description: " + deviceIn.getDeviceInfo().getDescription());
+                if (deviceIn.getMaxTransmitters() != 0) {
+                    // print device transmitter info
+                    System.out.println("~~Device Transmitters~~");
+                    System.out.println("Total Active: " + deviceIn.getTransmitters().size());
+                    System.out.println("Default: " + deviceIn.getTransmitter());
+                    midiIn  = deviceIn.getTransmitter();
+                }
             }
+        }
 
-            // get default sequencer and synthesizer
-            seq = MidiSystem.getSequencer();
-            syn = MidiSystem.getSynthesizer();
-
-            if (seq != null) System.out.println("Seq: " + seq.getDeviceInfo());
-            numTrans = seq.getTransmitters().size();
-            numRec = seq.getReceivers().size();
-            System.out.println("Num Trans: " + numTrans);
-            System.out.println("Num Rec: " + numRec);
-
-            if (syn != null) System.out.println("Syn: " + syn.getDeviceInfo());
-            numTrans = syn.getTransmitters().size();
-            numRec = syn.getReceivers().size();
-            System.out.println("Num Trans: " + numTrans);
-            System.out.println("Num Rec: " + numRec);
-
-            for (int i = 0; i < deviceList.size(); i++) {
-                // TODO: obtain device at position i from deviceList
-                System.out.println();
+        // find MIDI output device
+        for (int i = 0; i < infoArray.length; i++) {
+            deviceOut = MidiSystem.getMidiDevice(infoArray[i]);
+            // TODO: find better way to specify output device
+            if (deviceOut.getDeviceInfo().getName().contains("Microsoft GS Wavetable Synth")) {
+                System.out.println(deviceOut.toString());
+                // TODO: check if needs to open / delete both opens bc unnecessary (and closes?)
+                deviceOut.open();
+                // print device info
+                System.out.println("~~Device Info~~");
+                System.out.println("Name: " + deviceOut.getDeviceInfo().getName());
+                System.out.println("Description: " + deviceOut.getDeviceInfo().getDescription());
+                if (deviceOut.getMaxReceivers() != 0) {
+                    // print device receiver info
+                    System.out.println("~~Device Receivers~~");
+                    System.out.println("Total Active: " + deviceOut.getReceivers().size());
+                    System.out.println("Default: " + deviceOut.getReceiver());
+                    midiOut  = deviceOut.getReceiver();
+                }
             }
+            System.out.println();
+        }
 
-//
-//                //if any transmitting devices were found
-//                if(deviceList.size() > 0) {
-//                    //for each device
-//                    for(int i = 0; i < deviceList.size(); i++) {
-//                        try {
-//                            //get all transmitters
-//                            List<Transmitter> transmitters = deviceList.get(i).getTransmitters();
-//                            //and for each transmitter
-//                            for(int j = 0; j<transmitters.size();j++) {
-//                                //create a new receiver
-//                                transmitters.get(i).setReceiver(
-//                                        deviceList.get(i).getReceiver()
-//                                );
-//                            }
-//                            //open each device
-//                            deviceList.get(i).open();
-//                            System.out.println(deviceList.get(i).getDeviceInfo()+" Was Opened");
-//                        } catch (MidiUnavailableException e) {}
-//                    }
+        System.out.println();
+
+        // TODO: while loop
+        while (deviceIn.isOpen() && deviceOut.isOpen()) {
+
+            midiIn.setReceiver(midiOut);
+            //System.out.println(midiOut);
+
+//            Sequencer seqr;
+//            seqr.addMetaEventListener(new MetaEventListener() {
+//                @Override
+//                public void meta(MetaMessage meta) {
+//                    System.out.println("Input Received!");
 //                }
-        } finally {
+//            });
+//            seqr.addControllerEventListener(
+//                    new ControllerEventListener() {
+//                        public void controlChange(ShortMessage event) {
+//                            System.out.println(event.getData1());
+//                            System.out.println(event.getData2());
+//                        }
+//                    }
+//            )
 
         }
+
+        deviceIn.close();
+        deviceOut.close();
 
     }
 
