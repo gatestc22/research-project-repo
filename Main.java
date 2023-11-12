0ac7038a67b0fe70928e7df5d982d447b1256106
gatestc22 <gatestc22@gcc.edu>
2023-11-12 02:00:26 -0500

    Remove some unused code.

--- Main.java
+++ Main.java
@@ -25,17 +25,9 @@ public class Main {
         for (int i = 0; i < infoArray.length; i++) {
             deviceIn = MidiSystem.getMidiDevice(infoArray[i]);
             if (deviceIn.toString().contains("MidiInDevice")) {
-                System.out.println(deviceIn.toString());
-                //deviceIn.open();
-                // print device info
-                System.out.println("~~Device Info~~");
-                System.out.println("Name: " + deviceIn.getDeviceInfo().getName());
-                System.out.println("Description: " + deviceIn.getDeviceInfo().getDescription());
+                deviceIn.open();
+                System.out.println(deviceIn.toString() + " opened.");
                 if (deviceIn.getMaxTransmitters() != 0) {
-                    // print device transmitter info
-                    System.out.println("~~Device Transmitters~~");
-                    System.out.println("Total Active: " + deviceIn.getTransmitters().size());
-                    System.out.println("Default: " + deviceIn.getTransmitter());
                     midiIn  = deviceIn.getTransmitter();
                 }
             }
@@ -46,84 +38,50 @@ public class Main {
             deviceOut = MidiSystem.getMidiDevice(infoArray[i]);
             // TODO: find better way to specify output device
             if (deviceOut.getDeviceInfo().getName().contains("Microsoft GS Wavetable Synth")) {
-                System.out.println(deviceOut.toString());
                 // TODO: check if needs to open / delete both opens bc unnecessary (and closes?)
-                deviceOut.open();
-                // print device info
-                System.out.println("~~Device Info~~");
-                System.out.println("Name: " + deviceOut.getDeviceInfo().getName());
-                System.out.println("Description: " + deviceOut.getDeviceInfo().getDescription());
                 if (deviceOut.getMaxReceivers() != 0) {
-                    // print device receiver info
-                    System.out.println("~~Device Receivers~~");
-                    System.out.println("Total Active: " + deviceOut.getReceivers().size());
-                    System.out.println("Default: " + deviceOut.getReceiver());
                     midiOut  = deviceOut.getReceiver();
                 }
             }
-            System.out.println();
         }
 
-        System.out.println();
         seqr = MidiSystem.getSequencer();
-//        Transmitter transmitter;
-//        Receiver receiver;
+        seqr.open();
+        // transmitter: midiIn
+        // receiver: midiOut
+        Sequence seq = new Sequence(Sequence.PPQ, 24);
+        Track currentTrack = seq.createTrack();
+        seqr.setSequence(seq);
+        seqr.setTickPosition(0);
+        seqr.recordEnable(currentTrack, -1);
+        seqr.startRecording();
 
-        // TODO: while loop
-        while (deviceIn.isOpen() && deviceOut.isOpen()) {
-
-            midiIn.setReceiver(midiOut);
-            //System.out.println(midiOut);
-            seqr.addMetaEventListener(new MetaEventListener() {
-                @Override
-                public void meta(MetaMessage meta) {
-                    System.out.println("Input Received!");
-                }
-            });
-            seqr.addControllerEventListener(
-                    new ControllerEventListener() {
-                        public void controlChange(ShortMessage event) {
-                            System.out.println(event.getData1());
-                            System.out.println(event.getData2());
-                        }
-                    },
-                    getMidiFileTypes(seqr.getSequence())
-            );
-//
-//            // Open a connection to your input device
-//            deviceIn.open();
-//// Open a connection to the default sequencer (as specified by MidiSystem)
-//            seqr.open();
-//// Get the transmitter class from your input device
-//            transmitter = deviceIn.getTransmitter();
-//// Get the receiver class from your sequencer
-//            receiver = seqr.getReceiver();
-//// Bind the transmitter to the receiver so the receiver gets input from the transmitter
-//            transmitter.setReceiver(receiver);
-//
-//// And of course a track to record the input on
-//            Track currentTrack = seqr.getSequence().createTrack();
-//// Do some sequencer settings
-//            seqr.setSequence(seqr.getSequence());
-//            seqr.setTickPosition(0);
-//            seqr.recordEnable(currentTrack, -1);
-//// And start recording
-//            seqr.startRecording();
-//
-//            // Stop recording
-//            if(seqr.isRecording())
-//            {
-//                // Tell sequencer to stop recording
-//                //sequencer.stopRecording();
-//
-//                // Retrieve the sequence containing the stuff you played on the MIDI instrument
-//                Sequence tmp = seqr.getSequence();
-//
-//                // Save to file
-//                MidiSystem.write(tmp, 0, new File("MyMidiFile.mid"));
-//            }
-//
+        midiIn.setReceiver(midiOut);
+        while (deviceIn.isOpen()) {
         }
+
+
+
+//        // TODO: while loop
+//        while (deviceIn.isOpen() && deviceOut.isOpen()) {
+//
+//            seqr.addMetaEventListener(new MetaEventListener() {
+//                @Override
+//                public void meta(MetaMessage meta) {
+//                    System.out.println(meta.getData());
+//                }
+//            });
+//            seqr.addControllerEventListener(
+//                    new ControllerEventListener() {
+//                        public void controlChange(ShortMessage event) {
+//                            System.out.println(event.getData1());
+//                            System.out.println(event.getData2());
+//                        }
+//                    },
+//                    getMidiFileTypes(seqr.getSequence())
+//            );
+//
+//        }
 
         deviceIn.close();
         deviceOut.close();
